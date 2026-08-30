a57f81266fbd06f604ddc3334577aa8da2ae4a8f
Gihad Yasser <gihad12az@gmail.com>
2026-08-30 14:07:01 +0300

    tasks-s10

---
+++ Session 10/tast2/JS/Script.js
@@ -0,0 +1,18 @@
+function looping(startNum, endNum, breakNum, contNum) {
+    if ( startNum === undefined || endNum === undefined || breakNum === undefined || contNum === undefined) {
+        console.log("Please enter your numbers");
+        return;
+    } 
+    else{
+      for (var i = startNum; i <= endNum; i++) {
+        if (i === breakNum) {
+            break;
+        }
+        if (i === contNum) {
+            continue;
+        }
+        console.log(i);
+      }  
+    }
+}
+looping(1, 10, 8, 3);
